7f004adb5190fdaa05a446c39d5d0659bcee28a3
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-22 19:44:20 +0330

    ♻️ Add signin page. Optionset title for seo.

--- techonit-desk/pages/_app.js
+++ techonit-desk/pages/_app.js
@@ -1,17 +1,28 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import AuthLayout from '../components/auth/layouts/layout'
 import MainLayout from '../components/main/layouts/layout'
 import ManageLayout from '../components/manage/layouts/layout'
 
+import '../styles/globals.css'
+
 function MyApp({ Component, pageProps }) {
   // set layout
-  const Layout = Component.layout === 'auth' ? AuthLayout : 
-  Component.layout === 'manage' ? ManageLayout : MainLayout;
+  const Layout = Component.layout === 'auth' ? AuthLayout :
+    Component.layout === 'manage' ? ManageLayout : MainLayout;
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Technology On It {Component.title ? ' - ' + Component.title : ''}</title>
+        if(Component.description){
+          <meta name="description" content={Component.description} />
+        }
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
---
+++ techonit-desk/pages/auth/signin.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link';
+export default function Signin() {
+    return (
+        <>
+            <div className="flex flex-col justify-center h-[100%] px-8 md:px-[20%] lg:px-[30%]">
+                {/* Image And Text */}
+                <div className="text-center flex flex-col mb-12">
+                    <div className='grid mb-3'>
+                        <Image src="/assets/Sun rise-cuate.svg" alt="Nature view" width={100} height={200} />
+                    </div>
+                    <div>
+                        <label className="text-primary-500 text-2xl font-bold">Welcome</label>
+                        <p>Enter your phone number to signin or signup!</p>
+                    </div>
+                </div>
+                {/* Form And Button */}
+                <div>
+                    <div className="mb-9">
+                        <input
+                            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                            maxLength={11}
+                            type='text'
+                            placeholder='Phone Number' />
+                    </div>
+                    <div className='text-center flex justify-center gap-3'>
+                        <Link href='/'>
+                            <a className='px-3 py-2'>Back</a>
+                        </Link>
+                        <button className="bg-gradient-to-tr from-primary-100 to-primary-500 px-7 py-2 rounded-lg">Login</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+Signin.layout = 'auth';
+Signin.title = 'Login or create account!';
