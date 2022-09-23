763438e94f2ffd7cd7a3180e0b25fbbe981968c9
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 23:22:22 +0330

    🌱 Device card control.

--- techonit-desk/pages/manage/index.js
+++ techonit-desk/pages/manage/index.js
@@ -1,10 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import DeviceCard from '../../components/manage/device/deviceCard'
 
 export default function Index() {
     return (
         <>
-            Overview
+            <div className='mb-5'>
+                <label className='text-xl font-extrabold'>Overview</label>
+            </div>
+
+            <div>
+                <label className='flex mb-3 text-lg font-semibold'>Devices</label>
+                <div className='flex flex-wrap gap-3'>
+                    <DeviceCard icon='far fa-lightbulb' name='TV Main' />
+                    <DeviceCard icon='far fa-wifi' name='Wifi' />
+                </div>
+            </div>
         </>
     )
 }
