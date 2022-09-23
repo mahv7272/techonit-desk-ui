3187126a6a920d1c7e2bdc2087e064a08fc269a8
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 19:45:30 +0330

    🌱 Add overview page.

---
+++ techonit-desk/pages/manage/index.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Index() {
+    return (
+        <>
+            Overview
+        </>
+    )
+}
+
+Index.layout = 'manage';
+Index.title = 'Overview';
