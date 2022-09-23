581e54a9407211cec0fe540fe3505b08d42b2993
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 19:42:24 +0330

    🌱 Add profile page.

---
+++ techonit-desk/pages/manage/profile/index.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Index() {
+    return (
+        <>
+            My Profile
+        </>
+    )
+}
+
+Index.layout = 'manage';
+Index.title = 'My profile information';
